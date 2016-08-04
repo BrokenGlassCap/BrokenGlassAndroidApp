d1c1c9f7e61c615e1de0d2cb2ed57cd38d0d4f37
Oleg <klemtsov@gmail.com>
2016-08-04 17:58:21 +0500

    add yandexmaps

---
+++ rorb/app/src/androidTest/java/ru/sbrf/zsb/android/rorb/AddressTest.java
@@ -0,0 +1,36 @@
+package ru.sbrf.zsb.android.rorb;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Администратор on 22.07.2016.
+ */
+public class AddressTest {
+
+    @org.junit.Test
+    public void testGetDistance() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetDistance() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetUpperInfo() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetTextDistance() throws Exception {
+
+    }
+}
